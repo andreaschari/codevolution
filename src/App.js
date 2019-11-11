2752e5357f4311791c73862707439cdb55d99bc0
Andreas Chari <2293299C@student.gla.ac.uk>
2019-11-11 20:39:05 +0000

    prop destructuring

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       <Hello/>
       <Message/>
+      <Greet name="James" surname="Bond"/>
       <Counter/>
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const Greet = () => <h1>Hello</h1>
+const Greet = ({name, surname}) =>{
+    return (<div>
+        <h1>Hello {name} {surname}</h1>
+    </div>)
+}
 
 export default Greet;
